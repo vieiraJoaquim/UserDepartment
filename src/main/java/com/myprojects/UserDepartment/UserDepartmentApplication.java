5b0b153a3bcef2e69ead99a654e25c9c6c67a3f2
Vieira Joaquim <vjoaquim1@gmail.com>
2023-02-03 10:27:40 -0300

    Projeto criado

---
+++ src/main/java/com/myprojects/UserDepartment/UserDepartmentApplication.java
@@ -0,0 +1,13 @@
+package com.myprojects.UserDepartment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDepartmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDepartmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myprojects/UserDepartment/UserDepartmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojects.UserDepartment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDepartmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
